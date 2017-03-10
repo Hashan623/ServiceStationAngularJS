48064159ef6dc449411ebba8a041011fba43f4a3
zuko623 <hashanbuddika555@gmail.com>
2017-03-10 10:49:53 +0530

    Initial

---
+++ ServiceStationAngularJS/ServiceStationAngularJS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServiceStationAngularJS.Startup))]
+namespace ServiceStationAngularJS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
